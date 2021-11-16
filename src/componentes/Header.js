a9a5ee6cd70c7d050ab4c9ed3da2903862e52bef
furquin <laert.ff@gmail.com>
2021-11-16 18:32:22 -0300

    cria links de navegaçao para pesquisar, favoritos e perfil

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -21,9 +22,29 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         {!user && <Loading />}
-        {user && <p data-testid="header-user-name">
-          { user.name }
-        </p> }
+        {user && (
+          <>
+            <p data-testid="header-user-name">
+              { user.name }
+            </p>
+
+            <nav>
+              <Link to="/search">
+                <p data-testid="link-to-search"> Pesquisar </p>
+              </Link>
+
+              <Link to="/favorites">
+                <p data-testid="link-to-favorites"> Favoritos </p>
+              </Link>
+
+              <Link to="/profile">
+                <p data-testid="link-to-profile"> Perfil </p>
+              </Link>
+
+            </nav>
+          </>
+        )}
+
       </header>
     );
   }
