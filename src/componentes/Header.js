dc04bd2ceaff3408938d3ae97a0b1b621c0c5870
furquin <laert.ff@gmail.com>
2021-11-16 17:58:06 -0300

    implementa funçao getUser

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: '',
+    };
+  }
+
+  componentDidMount() {
+    getUser()
+      .then((nameUser) => this.setState({ user: nameUser }));
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <header data-testid="header-component">
-        <span
-          data-testid="header-user-name"
-          onChange= { }
-        />
+        {!user && <Loading />}
+        {user && <p data-testid="header-user-name">
+          { user.name }
+        </p> }
       </header>
     );
   }
