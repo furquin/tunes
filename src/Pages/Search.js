ceb36f98bc39f13e856d63922a42b3480c48979a
furquin <laert.ff@gmail.com>
2021-11-17 20:32:20 -0300

    implementa componente search

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import Header from '../componentes/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchArtistInput: '',
+    };
+  }
+
+  onInputChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { searchArtistInput } = this.state;
+    const { onInputChange } = this;
+    const minCharacters = 2;
+    const disabledButton = searchArtistInput.length < minCharacters;
+
     return (
       <>
         <div data-testid="page-search" />
         <Header />
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name="searchArtistInput"
+          value={ searchArtistInput }
+          onChange={ onInputChange }
+        />
+
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          name="searchArtistButton"
+          disabled={ disabledButton }
+        >
+          Pesquisar
+        </button>
       </>
     );
   }
